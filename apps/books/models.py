ff540fb25c224ff7ae22d3c87233db9a473e2a2e
Troy Center <troycenter1@gmail.com>
2017-06-22 17:59:41 -0700

    finished. No CSS, just program.

--- apps/books/models.py
+++ apps/books/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return "id:" + str(self.id) + ", " + self.title + ", " + str(self.id)
